2a70dc3ac61a4a8e073fcfdca6ca5720babc9f2c
Abrasimov <abrasimov.dev@gmail.com>
2023-05-30 00:16:47 +0200

    Remove commented code

--- src/store/slices/githubDataSlice.js
+++ src/store/slices/githubDataSlice.js
@@ -55,25 +55,3 @@ const githubDataSlice = createSlice({
 });
 
 export default githubDataSlice.reducer;
-
-// const initialState = {
-//     searchQuery: "react",
-//     loading: true,
-// };
-//
-// export const githubDataSlice = createSlice({
-//     name: "githubData",
-//     initialState,
-//     reducers: {
-//         setSearchQuery: (state, action) => {
-//             state.searchQuery = action.payload;
-//         },
-//         setLoading: (state, action) => {
-//             state.loading = !!action.payload;
-//         },
-//     },
-// });
-//
-// export const { setSearchQuery, setLoading } = githubDataSlice.actions;
-//
-// export default githubDataSlice.reducer;
